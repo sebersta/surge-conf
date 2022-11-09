a1ea96ea793f7ea1f5de6b61cf28e8296e8c6478
Chien Chin <flounderqin@outlook.com>
2022-11-09 08:21:24 +0800

    Update snell.sh

--- scripts/snell.sh
+++ scripts/snell.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 apt-get install wget unzip -y
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl -p
+sysctl net.ipv4.tcp_available_congestion_control
 cd
 wget -c https://dl.nssurge.com/snell/snell-server-v4.0.0-linux-amd64.zip
 unzip -o snell-server-v4.0.0-linux-amd64.zip
